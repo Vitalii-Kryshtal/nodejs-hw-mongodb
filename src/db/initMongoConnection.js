e2c9312192b46000329c26066083792414d8beee
Vitalii Kryshtal <krishtal.vtalk@ukr.net>
2025-08-06 19:25:03 +0300

    hw

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -12,7 +12,7 @@ export const initMongoDBConnection = async () => {
   const clientOptions = {
     serverApi: { version: "1", strict: true, deprecationErrors: true },
   };
-  const uri = `mongodb+srv://krishtalvtalk:N9z2l9fEYC4abkXo@cluster0.cznbq0q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+  const uri = `mongodb+srv://${user}:${password}@${host}/${db}?retryWrites=true&w=majority&appName=Cluster0`;
 
   try {
     await mongoose.connect(uri, clientOptions);
