784a9970c22c7cd472d8536dce3b73de2d9c70ac
Vitalii Kryshtal <krishtal.vtalk@ukr.net>
2025-09-13 20:39:15 +0300

    hw

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -12,7 +12,7 @@ import {
   updateContactSchema,
 } from "../validation/contacts.js";
 import { validateBody } from "../middlewares/validateBody.js";
-import { isValidId } from "../middlewares/isValidId.js";
+import { isValidId } from "../middlewares/isValidid.js";
 import { authenticate } from "../middlewares/authenticate.js";
 import { upload } from "../middlewares/multer.js";
 
